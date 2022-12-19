64c08413e91993bc7d6ef42f24b2bb6bc7ef1735
Byongho96 <unlike96@gmail.com>
2022-12-19 18:05:50 +0900

    mysql

--- backend/kiosk/settings.py
+++ backend/kiosk/settings.py
@@ -143,12 +143,16 @@ WSGI_APPLICATION = 'kiosk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from . import my_settings
+
+DATABASES = my_settings.DATABASES
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
